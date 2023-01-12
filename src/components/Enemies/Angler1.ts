03ab27ccfca0f56fd8b2d8cd362750ac55d87c92
max-im <pogidaevmo@gmail.com>
2023-01-12 19:18:40 +0200

    add angler1 sprite

--- src/components/Enemies/Angler1.ts
+++ src/components/Enemies/Angler1.ts
@@ -7,13 +7,17 @@ export class Angler1 extends Enemy {
     height: number;
     score: number;
     lives: number;
+    image: HTMLImageElement;
+    frameY = Math.floor(Math.random() * 3);
+    maxFrame = 37;
 
     constructor(game: Game) {
         super(game);
-        this.width = 228 * 0.2;
-        this.height = 169 * 0.2;
+        this.width = 228;
+        this.height = 169;
         this.lives = 5;
         this.score = 5;
         this.y = Math.random() * (game.height * 0.9 - this.height);
+        this.image = <HTMLImageElement>document.getElementById('angler1');
     }
 }
--- src/components/Enemies/Enemy.ts
+++ src/components/Enemies/Enemy.ts
@@ -7,12 +7,16 @@ export abstract class Enemy implements IDrawable, IUpdatable {
     markedForDelete = false;
     speedX: number;
     x: number;
+    frameX = 0;
 
+    abstract maxFrame: number;
+    abstract frameY: number;
     abstract score: number;
     abstract lives: number;
     abstract y: number;
     abstract width: number;
     abstract height: number;
+    abstract image: HTMLImageElement;
     
     constructor(game: Game) {
         this.game = game;
@@ -23,14 +27,32 @@ export abstract class Enemy implements IDrawable, IUpdatable {
     update() {
         this.x += this.speedX;
         if (this.x + this.width < 0) this.markedForDelete = true;
+        if (this.frameX < this.maxFrame) {
+            this.frameX++;
+        } else {
+            this.frameX = 0;
+        }
     }
 
     draw() {
-        this.game.ctx.fillStyle = 'red';
-        this.game.ctx.fillRect(this.x, this.y, this.width, this.height);
-        // this.game.ctx.fillStyle = 'black';
-        // this.game.ctx.font = '20px Helvetica';
-        // this.game.ctx.fillText(this.lives, this.x, this.y);
+        this.game.ctx.drawImage(
+            this.image, 
+            this.frameX * this.width,
+            this.frameY * this.height,
+            this.width,
+            this.height,
+            this.x, 
+            this.y, 
+            this.width, 
+            this.height
+        );
+        if (this.game.debug) {
+            this.game.ctx.fillStyle = 'black';
+            this.game.ctx.strokeRect(this.x, this.y, this.width, this.height);
+            this.game.ctx.fillStyle = 'black';
+            this.game.ctx.font = '20px Helvetica';
+            this.game.ctx.fillText(this.lives.toString(), this.x, this.y);
+        }
         
     }
 }
